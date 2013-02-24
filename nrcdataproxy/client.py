fcc39d9fc004dbbf3504ddfa9a4b6b2f4b0d45c9
Tim Freund <tim@freunds.net>
2013-02-24 15:48:01 -0500

    A more robust save implementation than 'pass'... :-)

--- nrcdataproxy/client.py
+++ nrcdataproxy/client.py
@@ -1,7 +1,15 @@
+import json
+import requests
 
 class NRCDataClient:
     def __init__(self, url):
         self.url = url
+        self.default_headers = {'Content-type': 'application/json'}
 
     def save(self, incident):
-        pass
+        r = requests.put("%(url)s/incidents/%(incident_id)d" % {'incident_id':incident['seqnos'],
+                                                                'url': self.url},
+                         data=json.dumps(incident),
+                         headers = self.default_headers)
+        print "%d: %s" % (incident['seqnos'], r.status_code)
+
