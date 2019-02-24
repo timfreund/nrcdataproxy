a8c2e857958675ebf67bd2bde788e7bd69734161
Tim Freund <tim@freunds.net>
2019-02-24 13:06:39 -0500

    Move dependencies to requirements.txt
    We're using the git repo for agate-excel due to a breaking change
    in openpyxl.  To ensure that new users receive the correct version
    of agate-excel we've moved dependencies to requirements.txt so the
    git repository can be explicitly defined.

--- setup.py
+++ setup.py
@@ -11,18 +11,12 @@ setup(
     license = 'MIT',
     url='http://bitbucket.com/timfreund/NRCDataProxy',
     install_requires=[
-        'flask',
-        'nose',
-        'openpyxl',
-        'pymongo',
-        'requests',
-        'xlrd',
                 ],
     packages=['nrcdataproxy'],
     include_package_data=True,
     entry_points="""
     [console_scripts]
-    nrcdataproxy = nrcdataproxy:serve
+    nrcdataproxy = nrcdataproxy.web:serve
     nrcdata-etl-archive-download = nrcdataproxy.etl.bootstrap:spreadsheet_downloader
     nrcdata-etl-archive-extractor = nrcdataproxy.etl.spreadsheet:extractor_command
     nrcdata-geocoder = nrcdataproxy.maintenance:geocode_command
