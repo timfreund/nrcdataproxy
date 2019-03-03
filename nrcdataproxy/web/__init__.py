fd736a8cddfcd1faa8adaf7b9665bfe8d5bd3846
Tim Freund <tim@freunds.net>
2019-03-03 14:57:32 -0500

    remove mongo from the web proxy

--- nrcdataproxy/web/__init__.py
+++ nrcdataproxy/web/__init__.py
@@ -19,21 +19,22 @@ def save(seqnos):
     return "OK"
 
 def serve():
-
     parser = OptionParser(usage="usage: %%prog [options]\n%s" % serve.__doc__)
     parser.add_option(Option("--host", dest="host", default="0.0.0.0"))
-    parser.add_option(Option("--port", dest="port", default=5001, type="int"))
+    parser.add_option(Option("--port", dest="port", default=5000, type="int"))
     parser.add_option(Option("--debug", dest="debug", default=False, action="store_true"))
 
-    from nrcdataproxy.storage import MongoIncidentStore
-    for option in MongoIncidentStore.commandline_options():
-        parser.add_option(option)
+    # from nrcdataproxy.storage import MongoIncidentStore
+    # for option in MongoIncidentStore.commandline_options():
+    #     parser.add_option(option)
 
     (options, args) = parser.parse_args()
 
-    store = MongoIncidentStore.configure_from_commandline(options)
+    # store = MongoIncidentStore.configure_from_commandline(options)
 
-    webapp.config.store_class = MongoIncidentStore
+    from nrcdataproxy.storage import DebugStore
+
+    webapp.config.store_class = DebugStore
     webapp.config.store_config = options
 
     webapp.run(host=options.host,
