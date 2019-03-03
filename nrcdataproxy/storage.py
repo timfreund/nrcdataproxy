6a48fa7cc10f663bb234cc1e52d411b67d7b940e
Tim Freund <tim@freunds.net>
2019-03-03 14:55:34 -0500

    Extract old Mongo storage to its own file

--- nrcdataproxy/storage.py
+++ nrcdataproxy/storage.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from optparse import Option
-import pymongo
 
 class IncidentStore():
     def __init__(self, **kw):
@@ -53,39 +52,3 @@ class DebugStore(IncidentStore):
     def save(self, incident):
         log.write("Pretending to save %d\n" % incident['SEQNOS'])
 
-class MongoIncidentStore(IncidentStore):
-    def __init__(self, mongohost='localhost', mongoport=27017, database='nrc', **kw):
-        self.host = mongohost
-        self.port = mongoport
-        self.database_name = database
-        self.mongo_server = pymongo.Connection(self.host, self.port)
-        self.mongodb = self.mongo_server[self.database_name]
-        self.collection = self.mongodb.incidents
-
-    @classmethod
-    def configuration_options(klass):
-        return {'mongohost': 'localhost',
-                'mongoport': 27017,
-                'database': 'nrc'}
-
-    def save(self, incident):
-        r = {}
-        for k, v in incident.items():
-            value = v
-            if k.count('date_') > 0:
-                if v != '' and v is not None and not isinstance(v, datetime):
-                    value = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S')
-            elif v == u'':
-                value = None
-            elif type(v) == dict:
-                value = {}
-                for nested_k, nested_v in v.items():
-                    value[nested_k.lower()] = nested_v 
-
-            r[k.lower()] = value
-                
-        r['_id'] = int(incident['seqnos'])
-        self.collection.save(r)
-
-    def find(self, **kw):
-        self.collection.find(**kw)
